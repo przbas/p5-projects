24d7f53dd7194b5672ae92985615e5552f7b8e2d
przbas <przemyslaw.baskiewicz@gmail.com>
2024-01-28 22:06:22 +0100

    dodanie opisu osi i parametrow funkcji

--- fx-graph/sketch.js
+++ fx-graph/sketch.js
@@ -46,15 +46,19 @@ function setup() {
     stroke(0);
     for (let i = 1; i < ox/s; i++) {
       line(ox-i*s, h-oy+5, ox-i*s, h-oy-5);
+      text(i*(-1), ox-i*s-6, h-oy+20);
     }
     for (let i = 1; i < w-ox/s; i++) {
       line(ox+i*s, h-oy+5, ox+i*s, h-oy-5);
+      text(i, ox+i*s-4, h-oy+20);
     }
     for (let i = 1; i < oy/s; i++) {
       line(ox-5, h-oy+i*s, ox+5, h-oy+i*s);
+      text(i*(-1), ox-25, h-oy+i*s+5);
     }
     for (let i = 1; i < h-oy/s; i++) {
       line(ox-5, h-oy-i*s, ox+5, h-oy-i*s);
+      text(i, ox-25, h-oy-i*s+5);
     }
 
     // draw function
@@ -63,32 +67,17 @@ function setup() {
     //point(ox+x*s,h-oy-(y*s));
     strokeWeight(3);
 
-    // parabola ax2+bx+c
-    let a = 2, b = -12, c = 10;   
-    stroke(250, 0, 0);
+    let a = 0, b = 0, c = 0;
+
+    //fx
+    a = document.getElementById("fx-1").value*1;
+    b = document.getElementById("fx-2").value*1;
+    c = document.getElementById("fx-3").value*1;
+    stroke(20, 150, 0);
     for (let i = 0; i < w; i++) {
       x = (i-ox)/s;
       y = a*x*x +b*x +c;
       point(ox+x*s,h-oy-(y*s));
     }
-
-    // prosta ax+b
-    a = 0.5, b = -5;
-    stroke(0, 255, 0);
-    for (let i = 0; i < w; i++) {
-      x = (i-ox)/s;
-      y = a*x +b;
-      point(ox+x*s,h-oy-(y*s));
-    }
-
-    // hiperbola a/x
-    a = 5;
-    stroke(0, 255, 255);
-    for (let i = 0; i < w; i++) {
-      x = (i-ox)/s;
-      y = a/x;
-      point(ox+x*s,h-oy-(y*s));
-    }
     
-
   }
